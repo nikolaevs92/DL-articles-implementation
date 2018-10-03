ab61f931479fa26942824a994664fe2c9f0be511
nsa <snikolaev>
2018-10-03 18:01:25 +0300

    Add vgg11

--- models.py
+++ models.py
@@ -39,3 +39,330 @@ def alexnet(n_classes):
         nn.Dropout(),
         nn.Linear(4096, n_classes)
     )
+
+def vggA(n_classes):
+    '''
+    VGG16 implementation:
+    https://arxiv.org/pdf/1409.1556
+    '''
+    return nn.Sequential(
+        nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(2,2), stride=2, padding=0),
+        
+        nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(2,2), stride=2, padding=0),
+        
+        nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(2,2), stride=2, padding=0),
+        
+        nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(2,2), stride=2, padding=0),
+        
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(2,2), stride=2, padding=0),
+        
+        InOneDemensionReshape(),
+        
+        nn.Dropout(),
+        nn.Linear(512*7*7, 4096),
+        nn.ReLU(),
+        nn.Dropout(),
+        nn.Linear(4096, 4096),
+        nn.ReLU(),
+        nn.Dropout(),
+        nn.Linear(4096, 1024),
+        nn.ReLU(),
+        nn.Dropout(),
+        nn.Linear(1024, n_classes)
+    )
+
+def vggA_LRN(n_classes):
+    '''
+    VGG16 implementation:
+    https://arxiv.org/pdf/1409.1556
+    '''
+    return nn.Sequential(
+        nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.LocalResponseNorm(),
+        nn.MaxPool2d(kernel_size=(3,3), stride=2, padding=0),
+        
+        nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(3,3), stride=2, padding=0),
+        
+        nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(3,3), stride=2, padding=0),
+        
+        nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(3,3), stride=2, padding=0),
+        
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(3,3), stride=2, padding=0),
+        
+        InOneDemensionReshape(),
+        
+        nn.Dropout(),
+        nn.Linear(512*7*7, 4096),
+        nn.ReLU(),
+        nn.Dropout(),
+        nn.Linear(4096, 4096),
+        nn.ReLU(),
+        nn.Dropout(),
+        nn.Linear(4096, 1024),
+        nn.ReLU(),
+        nn.Dropout(),
+        nn.Linear(1024, n_classes)
+    )
+
+
+def vggB(n_classes):
+    '''
+    VGG16 implementation:
+    https://arxiv.org/pdf/1409.1556
+    '''
+    return nn.Sequential(
+        nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(3,3), stride=2, padding=0),
+        
+        nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(3,3), stride=2, padding=0),
+        
+        nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(3,3), stride=2, padding=0),
+        
+        nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(3,3), stride=2, padding=0),
+        
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(3,3), stride=2, padding=0),
+        
+        InOneDemensionReshape(),
+        
+        nn.Dropout(),
+        nn.Linear(512*7*7, 4096),
+        nn.ReLU(),
+        nn.Dropout(),
+        nn.Linear(4096, 4096),
+        nn.ReLU(),
+        nn.Dropout(),
+        nn.Linear(4096, 1024),
+        nn.ReLU(),
+        nn.Dropout(),
+        nn.Linear(1024, n_classes)
+    )
+
+
+def vggC(n_classes):
+    '''
+    VGG16 implementation:
+    https://arxiv.org/pdf/1409.1556
+    '''
+    return nn.Sequential(
+        nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(3,3), stride=2, padding=0),
+        
+        nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(3,3), stride=2, padding=0),
+        
+        nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(1,1), stride=1, padding=0),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(3,3), stride=2, padding=0),
+        
+        nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(1,1), stride=1, padding=0),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(3,3), stride=2, padding=0),
+        
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(1,1), stride=1, padding=0),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(3,3), stride=2, padding=0),
+        
+        InOneDemensionReshape(),
+        
+        nn.Dropout(),
+        nn.Linear(512*7*7, 4096),
+        nn.ReLU(),
+        nn.Dropout(),
+        nn.Linear(4096, 4096),
+        nn.ReLU(),
+        nn.Dropout(),
+        nn.Linear(4096, 1024),
+        nn.ReLU(),
+        nn.Dropout(),
+        nn.Linear(1024, n_classes)
+    )
+
+
+def vggD(n_classes):
+    '''
+    VGG16 implementation:
+    https://arxiv.org/pdf/1409.1556
+    '''
+    return nn.Sequential(
+        nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(3,3), stride=2, padding=0),
+        
+        nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(3,3), stride=2, padding=0),
+        
+        nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(3,3), stride=2, padding=0),
+        
+        nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(3,3), stride=2, padding=0),
+        
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(3,3), stride=2, padding=0),
+        
+        InOneDemensionReshape(),
+        
+        nn.Dropout(),
+        nn.Linear(512*7*7, 4096),
+        nn.ReLU(),
+        nn.Dropout(),
+        nn.Linear(4096, 4096),
+        nn.ReLU(),
+        nn.Dropout(),
+        nn.Linear(4096, 1024),
+        nn.ReLU(),
+        nn.Dropout(),
+        nn.Linear(1024, n_classes)
+    )
+
+
+def vggE(n_classes):
+    '''
+    VGG16 implementation:
+    https://arxiv.org/pdf/1409.1556
+    '''
+    return nn.Sequential(
+        nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(3,3), stride=2, padding=0),
+        
+        nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(3,3), stride=2, padding=0),
+        
+        nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(3,3), stride=2, padding=0),
+        
+        nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(3,3), stride=2, padding=0),
+        
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), stride=1, padding=1),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=(3,3), stride=2, padding=0),
+        
+        InOneDemensionReshape(),
+        
+        nn.Dropout(),
+        nn.Linear(512*7*7, 4096),
+        nn.ReLU(),
+        nn.Dropout(),
+        nn.Linear(4096, 4096),
+        nn.ReLU(),
+        nn.Dropout(),
+        nn.Linear(4096, 1024),
+        nn.ReLU(),
+        nn.Dropout(),
+        nn.Linear(1024, n_classes)
+    )
